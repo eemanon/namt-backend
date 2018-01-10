8608bbe1417e7966fd0c771a0ac09af173f19c9d
eemanon <mrklgrnd@yahoo.com>
2018-01-10 14:32:24 +0100

    example database connexion

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+
+    define('LOGIN','marc.fouque'); 
+    define('PASS','maynard4d');
+    define('SERVER','10.10.190.33'); 
+
+    define('BASE','marc.fouque_postgis');
+
+    
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+            require('connection.php'); 
+            $connection = pg_connect("dbname=".BASE." host=".SERVER." user=".LOGIN." password=".PASS); 
+			if(!$connection){echo"<h2>chiasse</h2>";}
+			$req = pg_query($connection,'SELECT * FROM tomato.Utilisateur'); 
+            if($req){
+				echo"<p>";
+				while ($a = pg_fetch_object($req)){//boucle sur tous les resultat obtenuent de la requete
+				
+							echo"$a->nom <br/>";
+					}
+                echo"</p>";
+            } 
+
+
+        ?>
+
+    </body></html>
