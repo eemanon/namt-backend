22473cdfbbcad4aa7a2270777604b803d4515449
idrissa.seydou-issaka <idrissa.seydou-issaka@mathinfo.i-univ-tlse2.fr>
2018-01-11 16:55:31 +0100

    profil codes php

--- getEvaluations.php
+++ getEvaluations.php
@@ -7,14 +7,15 @@
 						$req =  pg_query($connection,"SELECT * FROM tomato.Evaluation where evalue='$email'");
  						//Executon de la requete preparer
 			if($req){
-				$evaluations["jardin"] = array();
-				while ($evaluation = pg_fetch_object($req)){//boucle sur tous les resultat obtenuent de la requete
+				$evaluations["evaluation"] = array();
+				while ($eval = pg_fetch_object($req)){//boucle sur tous les resultat obtenuent de la requete
 					$evaluation = array();
-							$evaluation["nom"] = $evaluation->nom;
-							$evaluation["photo"] = $evaluation->photo;
-							$evaluation["description"] = $evaluation->description;
+							$evaluation["note"] = $eval->note;
+							$evaluation["commentaire"] = $eval->commentaire;
+							$evaluation["evaluateur"] = $eval->evaluateur;
+						
 					// push single vehicule into final response array
-							array_push($evaluations["$evaluation"], $evaluation);
+							array_push($evaluations["evaluation"], $evaluation);
 					 echo json_encode($evaluations);
 				}
 			}
