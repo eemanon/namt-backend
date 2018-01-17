c2e12ba6bd6f9fe807ac6acdde8a5453b9a9094a
idrissa.seydou-issaka <idrissa.seydou-issaka@mathinfo.i-univ-tlse2.fr>
2018-01-17 17:28:46 +0100

    correction json

--- getMessages.php
+++ getMessages.php
@@ -26,9 +26,13 @@
 									$message["commentaire"] = $msg->commentaire;
 									$message["demandeur"] = $msg->demandeur;
 									// push single vehicule into final response array
-
-								}
 								array_push($messages["message"], $message);
+								}
+
+								echo json_encode($messages);
+								//!!!-------------\\\
+								break;
+
 
 							}else{
 								$test["Reponse"] = "Verifier votre connexion en filaire";
@@ -36,9 +40,34 @@
 							}
 
 
+						}else{
+
+							$jardin = $a->id;
+
+							$req2 =  pg_query($connection,"SELECT * FROM tomato.\"demandeJardin\" where jardin='$jardin'");
+							//Executon de la requete preparer
+							if($req2){
+								$messages["message"] = array();
+								while ($msg = pg_fetch_object($req2)){//boucle sur tous les resultat obtenuent de la requete
+									$message = array();
+									$message["commentaire"] = $msg->commentaire;
+									$message["demandeur"] = $msg->demandeur;
+									// push single vehicule into final response array
+								array_push($messages["message"], $message);
+								}
+
+								echo json_encode($messages);
+								//!!!-------------\\\
+								break;
+							}
+
+
+
+
+
 						}
 					}
-					echo json_encode($messages);
+
 				}else{
 					$test["Reponse"] = "Verifier votre connexion en filaire";
 					echo json_encode ($test);
