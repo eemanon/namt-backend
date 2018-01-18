2c2f33e8ba487589ce6deff2f13b1f6c79598886
Marc <marc.fouque4@gmail.com>
2018-01-18 16:52:55 +0100

    filter

--- filtreJardin.php
+++ filtreJardin.php
@@ -5,14 +5,12 @@
             $connection = pg_connect("dbname=".BASE." host=".SERVER." user=".LOGIN." password=".PASS); 
 			if(!$connection){echo"pas de connection, passe en filaire mon grand";}
 			
-			if(isset($_GET['s'])){
+			if(isset($_GET['s']) and strlen($_GET['s'])!=0){
 			$s=$_GET['s'];
-			echo $s;
-			$req = pg_query($connection,"SELECT tomato.Jardin.id,tomato.Jardin.adresse,tomato.Jardin.info,tomato.Jardin.description,tomato.Jardin.photo,tomato.Jardin.nom,tomato.Jardin.ville,tomato.Jardin.code,tomato.utilisateur.pseudo,tomato.utilisateur.photo,ST_AsGeoJSON(geom) as geoJ FROM tomato.Jardin,tomato.utilisateur where (code='$s' or ville='$s' or proprio='$s' or adresse='$s') and tomato.Jardin.proprio=tomato.utilisateur.email"); 
+			$req = pg_query($connection,"SELECT tomato.Jardin.id,tomato.Jardin.icon,tomato.Jardin.adresse,tomato.Jardin.info,tomato.Jardin.description,tomato.Jardin.photo,tomato.Jardin.nom,tomato.Jardin.ville,tomato.Jardin.code,tomato.utilisateur.pseudo,tomato.utilisateur.photo as photoProprio,ST_AsGeoJSON(geom) as geoJ FROM tomato.Jardin,tomato.utilisateur where (code='$s' or ville='$s' or proprio='$s' or adresse='$s') and tomato.Jardin.proprio=tomato.utilisateur.email"); 
 			}
-			
 			else{
-			$pol="SELECT *,ST_AsGeoJSON(geom) as geoJ FROM  tomato.Jardin";
+			$pol="SELECT tomato.Jardin.id,tomato.Jardin.icon,tomato.Jardin.adresse,tomato.Jardin.info,tomato.Jardin.description,tomato.Jardin.photo,tomato.Jardin.nom,tomato.Jardin.ville,tomato.Jardin.code,tomato.utilisateur.pseudo,tomato.utilisateur.photo as photoProprio,ST_AsGeoJSON(geom) as geoJ FROM  tomato.Jardin, tomato.utilisateur where tomato.Jardin.proprio=tomato.utilisateur.email";
 			$req = pg_query($connection,$pol);
 			}
 			
@@ -27,7 +25,7 @@
             }
 			else{
 				echo "<b>Erreur dans l'exécution de la requête. </b><br/>";
-				echo "<b>Message de mySQL: </b>".mysqli_error($connexion);
+				echo "<b>Message de mySQL: </b>".pg_error($connexion);
 				}			
 			pg_close($connection);
         ?>
