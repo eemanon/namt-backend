cc5ca9aa85e5ca48ac6d5ae6c60a4269a54e8d4c
idrissa.seydou-issaka <idrissa.seydou-issaka@mathinfo.i-univ-tlse2.fr>
2018-01-18 12:06:47 +0100

    jardines

--- getJardines.php
+++ getJardines.php
@@ -6,7 +6,7 @@
 			if(isset($_GET['email'])){ //Champ identifiant et mot de passe remplis
 				require_once('connect.inc.php');
 				$email = $_GET['email'];
-				$req =  pg_query($connection,"SELECT J.nom, J.info, J.photo, U.pseudo  FROM tomato.Jardine Je, tomato.Jardin J,tomato.Utilisateur U where J.id=Je.jardin and J.proprio='$email' and J.proprio=U.email ");
+				$req =  pg_query($connection,"SELECT J.nom, J.info, J.photo,U.pseudo  FROM tomato.Jardine Je, tomato.Jardin J, tomato.Utilisateur U  where J.id=Je.jardin and Je.jardinier='$email' and U.email=J.proprio");
 				//Executon de la requete preparer
 				if($req){
 					$jardines = array();
